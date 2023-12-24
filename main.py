610c88320ab4f19974fcabf0008e0be06c1ec3f3
Theo Cesar <theocesar@live.com>
2023-12-24 18:49:54 -0300

    Refactoring the entire logic of the solution

--- main.py
+++ main.py
@@ -1,96 +1,79 @@
-print('''Qual arquivo deverá ser analisado?
-[] teste1
-[] teste2
-[] teste3
-[] teste4
-
-    Para utilizar um arquivo diferente, personalize previamente o arquivo vazio teste4 !
- ''')
-
-def final_validation():
-  # Checking the two conditions
-  if check and estado == 1:
-      for i in aux:
-          print(i, end = '')
-      print(': pertence')
-  else:
-      for i in aux:
-          print(i, end = '')
-      print(': não pertence')
+from enum import Enum
 
 
+class State(Enum):
+    # states
+    Zero = 0
+    One = 1
+    Two = 2
+    Three = 3
 
 
-# Selecting the txt file chosen by the user
-opcao = str(input('Nome do arquivo escolhido: '))
-with open(opcao + ".txt", 'r') as arquivo:
-  linha = int(arquivo.readline())
+def transitions():
+    # state transitions
+    return {
+        State.Zero: {'a': State.One, 'b': State.Three, 'c': State.Zero},
+        State.One: {'a': State.One, 'b': State.Two, 'c': State.One},
+        State.Two: {'a': State.One, 'b': State.Three, 'c': State.Two},
+        State.Three: {'a': State.One, 'b': State.Three, 'c': State.Three}
+    }
 
-  while linha > 0:
-      aux = []
-      frase = arquivo.readline()
-      check = True
-      estado = -2
 
-      for char in list(frase):
-  
-          # initial state
-          if estado == -2:
-              if char == 'a':
-                  aux.append(char)
-                  estado = -1
-              if char == 'b':
-                  aux.append(char)
-                  estado = 1
-              if char == 'c':
-                  aux.append(char)
-                  estado = -2
-  
-          # second state
-          elif estado == -1:
-              if char == 'a':
-                  estado = -1
-                  aux.append(char)
-                  check = False
-              if char == 'b':
-                  estado = 0
-                  aux.append(char)
-              if char == 'c':
-                  aux.append(char)
-                  estado = -1
-  
-          # third state
-          elif estado == 0:
-              if char == 'a':
-                  estado = -1
-                  aux.append(char)
-                  check = False
-              if char == 'b':
-                  estado = 1
-                  aux.append(char)
-              if char == 'c':
-                  aux.append(char)
-                  estado = 0
-  
-          # forth state
-          elif estado == 1:
-              if char == 'a':
-                  estado = -1
-                  aux.append(char)
-              if char == 'b':
-                  estado = 1
-                  aux.append(char)
-              if char == 'c':
-                  aux.append(char)
-                  estado = 1
-  
-      linha -= 1
-  
-      # There are two check marks to validate the expression.
-      # One: The final state of FSM.
-      # Two: The boolean value of the variable check
-      # The final state of the FSM must be 1 and the boolean value must be True.
-      # This is the only way an expression can be correct.
-  
-      final_validation()
-  
+def validation(string):
+    # primary validation
+    transicoes = transitions()
+    estado = State.Zero
+    check = True
+    aux = []
+
+    for char in string:
+      aux.append(char)
+
+      # Checking if the character is valid
+      if char in transicoes[estado]:
+
+        # Validating e making the transitions
+        if char == 'a' and estado == State.One or char == 'a' and estado == State.Two:
+          check = False
+          estado = transicoes[estado][char]
+        else:
+          estado = transicoes[estado][char]
+
+    return aux, check, estado
+
+
+def final_validation(check, estado, aux):
+    # Checking if the expression is correct
+    result = ''.join(aux)
+
+    if check and estado == State.Three:
+        print(f'{result}: pertence')
+    else:
+        print(f'{result}: não pertence')
+
+
+def main():
+    print('''Qual arquivo deverá ser analisado?
+    [] teste1
+    [] teste2
+    [] teste3
+    [] teste4
+
+        Para utilizar um arquivo diferente, personalize previamente o arquivo vazio teste4 !
+    ''')
+
+    # Selecting the txt file chosen by the user
+    opcao = str(input('Nome do arquivo escolhido: '))
+    with open(opcao + ".txt", 'r') as arquivo:
+        linha = int(arquivo.readline())
+
+        while linha > 0:
+            frase = arquivo.readline().strip()
+            aux, check, estado = validation(frase)
+            final_validation(check, estado, aux)
+
+            linha -= 1
+
+
+if __name__ == "__main__":
+    main()
